f533b4d498ce7464f95f146728fefdf007c0a01f
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-21 11:24:17 +0300

    ContinueWith

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskConsoleApp
+{
+    internal class Program
+    {
+        private async  static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //ContinueWith parantez içinde ActionDelege istemektedir.
+            // Not: ücretsiz delege kursunuz izle
+            var myTask =new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(
+                ((data) =>
+                {
+                    Console.WriteLine("data uzunluk" +" "+ data.Result.Length);
+                }));
+
+            Console.WriteLine("Arada yapılacak işler");
+
+            await myTask;
+        }
+    }
+}

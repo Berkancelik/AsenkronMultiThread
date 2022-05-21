3f5c2aa66c828902e22b644572e9434949de440a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-21 13:01:53 +0300

    WaitAll

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,26 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskConsoleApp
 {
+    public class Content
+    {
+        public string Site { get; set; }
+        public int Length { get; set; }
+    }
     internal class Program
     {
-        private async  static Task Main(string[] args)
+        private async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Main thread :" + Thread.CurrentThread.ManagedThreadId);
+            List<string> urlsList = new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.n11.com",
+                "https://www.haberturk.com"
+            };
+            
+            List<Task<Content>> taskList = new List<Task<Content>>();
 
-            //ContinueWith parantez içinde ActionDelege istemektedir.
-            // Not: ücretsiz delege kursunuz izle
-            var myTask =new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(
-                ((data) =>
-                {
-                    Console.WriteLine("data uzunluk" +" "+ data.Result.Length);
-                }));
+            urlsList.ToList().ForEach(x =>
+            {
+                taskList.Add(GetContentAsync(x));
+            });
 
-            Console.WriteLine("Arada yapılacak işler");
+            Console.WriteLine("WaitAll methodundan önce");
+            bool result = Task.WaitAll(taskList.ToArray(),300);
+            Console.WriteLine("3 saniiye de geldi mi " + result);
+            Console.WriteLine("WaitAll methodundan sonra");
+            Console.WriteLine($"{taskList.First().Result.Site} - {taskList.First().Result.Length}");
 
-            await myTask;
+            
+        }
+        public static async Task<Content> GetContentAsync(string url)
+        {
+            Content content = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
+            content.Site = url;
+            content.Length = data.Length;
+            // Thread.CurrentThread.ManagedThreadId); çalışan thread'in id'sini almaktadır.
+            Console.WriteLine("GetContentAsync theread:" + Thread.CurrentThread.ManagedThreadId);
+            return content;
         }
     }
+
+
 }

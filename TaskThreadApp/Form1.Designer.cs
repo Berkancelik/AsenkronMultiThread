641e6e02013b38b9f3b5e47215c4122f69040c34
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-22 07:11:09 +0300

    Run()

---
+++ TaskThreadApp/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TaskThreadApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.baslat = new System.Windows.Forms.Button();
+            this.Sayac = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(121, 61);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(803, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(121, 164);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(803, 23);
+            this.progressBar2.TabIndex = 0;
+            // 
+            // baslat
+            // 
+            this.baslat.Location = new System.Drawing.Point(121, 12);
+            this.baslat.Name = "baslat";
+            this.baslat.Size = new System.Drawing.Size(75, 23);
+            this.baslat.TabIndex = 1;
+            this.baslat.Text = "Başlat";
+            this.baslat.UseVisualStyleBackColor = true;
+            this.baslat.Click += new System.EventHandler(this.baslat_Click);
+            // 
+            // Sayac
+            // 
+            this.Sayac.Location = new System.Drawing.Point(247, 12);
+            this.Sayac.Name = "Sayac";
+            this.Sayac.Size = new System.Drawing.Size(75, 23);
+            this.Sayac.TabIndex = 2;
+            this.Sayac.Text = "Sayaç";
+            this.Sayac.UseVisualStyleBackColor = true;
+            this.Sayac.Click += new System.EventHandler(this.Sayac_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 450);
+            this.Controls.Add(this.Sayac);
+            this.Controls.Add(this.baslat);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Button baslat;
+        private System.Windows.Forms.Button Sayac;
+    }
+}
+
---
+++ TaskThreadApp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TaskThreadApp
+{
+    public partial class Form1 : Form
+    {
+        public static int Counter { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void baslat_Click(object sender, EventArgs e)
+        {
+            await Go(progressBar1);
+            await Go(progressBar2);
+        }
+
+        private void Sayac_Click(object sender, EventArgs e)
+        {
+            Sayac.Text = Counter.ToString();
+        }
+
+        public async Task Go(System.Windows.Forms.ProgressBar pb)
+        {
+            await Task.Run(() =>{ Enumerable.Range(1, 100).ToList().ForEach(x =>
+            {
+              //  Thread.Sleep();
+                pb.Invoke((MethodInvoker)delegate { pb.Value = x; });
+            });
+        });
+    }
+}}

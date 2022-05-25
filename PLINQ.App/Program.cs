3ab06acfabf555e1af7a344d702c6b8c872aa3ca
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-26 01:06:03 +0300

    Write a PLINQ query on the entity

--- PLINQ.App/Program.cs
+++ PLINQ.App/Program.cs
@@ -8,14 +8,36 @@ namespace PLINQ.App
     internal class Program
     {
       
+        private static void WriteLog(Product p)
+        {
+            // log a yazdık
+            Console.WriteLine(p.Name + " Log'a KAydedildi");
+        }
 
         static void Main(string[] args)
         {
             AdventureWorks2017Context context = new AdventureWorks2017Context();
 
-            context.Products.Take(10).ToList().ForEach(x =>
+            // elimde var olan array üzerinde bir sogu yazmak ister isek
+            //complex sorgular var ise aşağıdaki sorgu çeşidi kullanılmaktadır.
+            // kod kadar okunabilirlik de çk önemlidir.
+            // SQL Server sorgusu
+            //var product = (from p in context.Products.AsParallel()
+            //              where p.ListPrice >10M
+            //              select p).Take(10);
+
+            // Tek satır sorgu
+            //var product2 = context.Products.AsParallel().Where(p => p.ListPrice >10M).Take(10);
+
+
+            //product.ForAll(x =>
+            //{
+            //    Console.WriteLine(x.Name);
+            //});
+
+            context.Products.AsParallel().ForAll(p =>
             {
-                Console.WriteLine(x.Name);
+                WriteLog(p);
             });
 
 
